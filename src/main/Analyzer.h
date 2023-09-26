5be27bbb91c1fa01601c9cd9b9698f1e765236ff
Edgar Flores <luisedgarflores@ciencias.unam.mx>
2023-09-25 19:37:56 -0600

    Analyzer sorts | write in main

--- src/main/Analyzer.h
+++ src/main/Analyzer.h
@@ -1,14 +1,85 @@
+#include <iostream>
+#include <fstream>
 #include <string>
 #include <list>
-#include <iostream>
+#include <tuple>
+#include <dirent.h>
+#include <cmath>
 
 class Analyzer {
-    private:
+private:
+    static std::list<std::tuple<std::string, std::string>>
+        readFilesInDirectory(std::string dirpath) {
 
+        std::list<std::tuple<std::string, std::string>> fileContents;
+
+        DIR* dir = opendir(dirpath.c_str());
+        if (!dir) {
+            std::cerr << "Failed to open directory: " << dirpath << std::endl;
+            return fileContents;
+        }
+
+        struct dirent* entry;
+        while ((entry = readdir(dir))) {
+            std::string filePath = dirpath + "/" + entry->d_name;
+            std::ifstream file(filePath);
+            if (entry->d_type != DT_REG)
+                continue;
+            if (file.is_open()) {
+                std::string firstLine;
+                std::getline(file, firstLine);
+
+                std::string content;
+                std::string line;
+                while (std::getline(file, line)) {
+                    content += line + "\n";
+                }
+
+                file.close();
+                fileContents.push_back(std::make_tuple(firstLine, content));
+            } else {
+                std::cerr << "Failed to open file: " << filePath << std::endl;
+            }
+        }
+
+        closedir(dir);
+
+        return fileContents;
+
+    }
+    static bool comp_tup(std::tuple<std::string, std::string> a, std::tuple<std::string, std::string> b) {
+        double evalA = quit_eval(std::get<0>(a));
+        double evalB = quit_eval(std::get<0>(b));
+        return evalA < evalB;
+    }
+
+    static double quit_eval(std::string eval){
+
+        std::string substr = "Eval : ";
+
+        size_t position = eval.find(substr);
+
+        if (position != std::string::npos) {
+            eval.erase(position, substr.length());
+        } else {
+            std::cout << "Substring not found in the original string." << std::endl;
+        }
+        return std::stod(eval);
+    }
     public:
 
     static void sort(std::string dir){
+        std::list<std::tuple<std::string, std::string>> fileContents
+            = readFilesInDirectory(dir);
 
+        fileContents.sort(comp_tup);
+
+        int firstpercent = std::round(fileContents.size()/10);
+        fileContents.resize(firstpercent);
+
+        for (auto tuple : fileContents) {
+            std::cout << "First Line: " << std::get<0>(tuple) << std::endl;
+            std::cout << "Content:\n" << std::get<1>(tuple) << std::endl;
+        }
     }
-
 };
--- src/main/execs/Create_sols.cpp
+++ src/main/execs/Create_sols.cpp
@@ -13,6 +13,26 @@ bool directoryExists(const std::string& path) {
     return stat(path.c_str(), &st) == 0 && S_ISDIR(st.st_mode);
 }
 
+
+void write(std::shared_ptr<Heuristic> h,
+           std::shared_ptr<Instance> instance,
+           std::ofstream& outputFile, int size, double eval,
+           std::shared_ptr<City>* min){
+    outputFile << "Eval : "  << eval << std::endl;
+    outputFile << "SEED : "  << instance->get_seed() << std::endl;
+    outputFile << "Lot  : "  << h->get_lotes() << std::endl;
+    outputFile << "Temp : "  << h->get_temp() << std::endl;
+    outputFile << "Eps  : "  << h->get_eps() << std::endl;
+    outputFile << "EpsT : "  << h->get_eps_temp() << std::endl;
+    outputFile << "Phi  : "  << h->get_phi() << std::endl;
+    for(int i = 0; i<size; i++){
+        outputFile << min[i]-> get_id();
+        if(i != size-1)
+            outputFile << ",";
+    }
+
+}
+
 bool removeDirectory(const std::string& path) {
     DIR* dir = opendir(path.c_str());
     if (dir) {
@@ -110,28 +130,21 @@ int main(int argc, char *argv[]) {
         std::shared_ptr<Heuristic> h
             = std::make_shared<Heuristic>(instance, lotes, temp, eps, epsP, phi, size, false);
 
-        auto [first, min] = h->apu();
 
-        if(flag == "-s")
+        auto [first, min] = h->apu();
+        double eval = instance->eval(min);
+
+        outputFile << std::setprecision(16);
+        if(flag == "-s"){
             i++;
+            write(h, instance, outputFile, size, eval, min);
+        }
 
         c++;
         if(flag == "-f")
             if (instance->eval(min) < 1){
                 i++;
-                outputFile << std::setprecision(16);
-                outputFile << "Eval : "  << instance->eval(min) << std::endl;
-                outputFile << "SEED : "  << instance->get_seed() << std::endl;
-                outputFile << "Lot  : "  << h->get_lotes() << std::endl;
-                outputFile << "Temp : "  << h->get_temp() << std::endl;
-                outputFile << "Eps  : "  << h->get_eps() << std::endl;
-                outputFile << "EpsT : "  << h->get_eps_temp() << std::endl;
-                outputFile << "Phi  : "  << h->get_phi() << std::endl;
-                for(int i = 0; i<size; i++){
-                    outputFile << min[i]-> get_id();
-                    if(i != size-1)
-                        outputFile << ",";
-                }
+                write(h, instance, outputFile, size, eval, min);
             }
 
         outputFile.close();
