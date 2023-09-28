a28857289cb1ee846588606512ff81f504b8f6ce
Edgar Flores <luisedgarflores@ciencias.unam.mx>
2023-09-27 22:39:42 -0600

    print XD main

--- src/main/Instance.h
+++ src/main/Instance.h
@@ -44,6 +44,7 @@ class Instance {
         }
 
         void complete() {
+            std::cout << "XD" << std::endl;
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
                     int city1_id = sol[i]->get_id();
