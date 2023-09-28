d77e7cb1aaf13a4cf48738acfa4684c3777a3046
Edgar Flores <luisedgarflores@ciencias.unam.mx>
2023-09-27 23:11:54 -0600

    remove xd print

--- src/main/Instance.h
+++ src/main/Instance.h
@@ -44,7 +44,6 @@ class Instance {
         }
 
         void complete() {
-            std::cout << "XD" << std::endl;
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
                     int city1_id = sol[i]->get_id();
